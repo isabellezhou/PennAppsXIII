bf4394029c324f9475829ee638588b5706a2ba4d
Isabelle Zhou <isabelle.jq.zhou@gmail.com>
2016-01-23 22:56:46 -0500

    Added code.

--- www/index.js
+++ www/index.js
@@ -1,12 +1,5 @@
-$(document).ready(function() {
-  $("#arrow").click(function() {
-      $('html,body').animate({
-          scrollTop: $(".arrow").offset().top-100},
-          'slow');
-});
-});
-
-$(document).ready(function(){ 
-    setTimeout(function(){ 
-        $("div.landing").fadeIn();}, 2000);
+$(document).ready(function(){
+  $("#arrow").click(function(){
+    $("div.landing").hide();
+  });
 });
